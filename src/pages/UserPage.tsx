881074c5b9ec2024f3f880c9eb51e8c70b519bdc
[dyad] <gokdenizaltun522@gmail.com>
2025-09-21 22:43:47 +0300

    Updated src/pages/UserPage.tsx

--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -42,15 +42,16 @@ const src = `https://www.youtube-nocookie.com/embed/${videoId}?autoplay=1&mute=1
     : 'absolute w-0 h-0 border-0 -z-10';
 
   return (
-    <iframe
-      className={playerClasses}
-      src={src}
-      title="YouTube video player"
-      frameBorder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-      referrerPolicy="strict-origin-when-cross-origin"
-      allowFullScreen
-    ></iframe>
+  <iframe
+    className={playerClasses}
+    src={`https://www.youtube-nocookie.com/embed/${videoId}?autoplay=1&mute=1&controls=0&modestbranding=1&rel=0&iv_load_policy=3`}
+    title="YouTube video player"
+    frameBorder="0"
+    allow="autoplay; accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+    referrerPolicy="strict-origin-when-cross-origin"
+    allowFullScreen
+  ></iframe>
+
   );
 };
 
