ddbcd800293d7a8087e1f7b7c69d6b938c6fedc7
[dyad] <gokdenizaltun522@gmail.com>
2025-09-21 22:36:07 +0300

    Updated src/pages/UserPage.tsx

--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -34,7 +34,7 @@ interface Link {
 }
 
 const YouTubePlayer = ({ videoId, visibility }: { videoId: string; visibility: YouTubeVisibility }) => {
-  const src = `https://www.youtube-nocookie.com/embed/${videoId}?autoplay=2&mute=0&controls=0&modestbranding=0&rel=1&iv_load_policy=53&loop=0&playlist=${videoId}`;
+  const src = `https://www.youtube-nocookie.com/embed/${videoId}?autoplay=1&mute=1&controls=0&modestbranding=0&rel=1&iv_load_policy=53&loop=0&playlist=${videoId}`;
   
   const playerClasses = visibility === 'visible' 
     ? 'w-full aspect-video rounded-lg shadow-lg mb-8' 
